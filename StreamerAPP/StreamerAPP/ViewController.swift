693316bce8bb0061c5f3c2f9e7c1b489ca74875a
ZanJereb <jereb-mac@Jereb-macs-MacBook-Pro.local>
2017-02-17 09:30:07 +0100

    Created app directory

---
+++ StreamerAPP/StreamerAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamerAPP
+//
+//  Created by Jereb-mac on 12/02/2017.
+//  Copyright © 2017 Jereb-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
